5132536d870d63b21eb030c7a72108006ea370e1
moonstar3109 <74219160+moonstar3109@users.noreply.github.com>
2022-06-02 13:42:03 +0900

    비밀 문서

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // HEROKU에 적용한 이름 그대로 
+    mongoURI : process.env.MONGO_URI
+}
--- index.js
+++ index.js
@@ -5,27 +5,33 @@ const mongoose = require('mongoose');
 const { User } = require('./models/User')
 const bodyParser = require('body-parser')
 
+const config = require('./config/key');
+
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extend:true}));
 
 // application/json
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://moonstar:dlwnsgus12@learnnode.x4vmhxs.mongodb.net/?retryWrites=true&w=majority').then(() => console.log('MonogoDB Connect'))
+mongoose.connect(config.mongoURI).then(() => console.log('MonogoDB Connect'))
   .catch(err => console.log('Connect error : '+ err))
 
 app.get('/', (req,res) => {
-    res.send('Hell')
+    res.send('dddfaf')
 })
 
 app.post('/register', (req,res) => {
     // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
     // 그것들을 데이터 베이스에 넣어준다
 
+    // request 에서 넘어온 body의 내용을 user 변수에 저장
     const user = new User(req.body)
 
+    // save는 몽구스 내장 함수
     user.save( (err, userInfo) => {
-      if(err) return res.json({success : false, message : err})
+      if(err) {
+        return res.json({success : false, message : err})
+      }
       return res.status(200).json({
         success : true
       })
